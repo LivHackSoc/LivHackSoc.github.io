76da0fb0301def807fef54abeb88930ee7fa9849
Robert Szafarczyk <robertszafa@gmail.com>
2018-06-05 21:37:05 +0100

    navigation bar stays fixed when scrolling. All labels are hidden except the overlay and our favicon. Favicon brings user back to landing page when clicked

--- src/overlay-menu.js
+++ src/overlay-menu.js
@@ -1,8 +1,49 @@
-// Open the menu by display the 
+// Open the menu by display the
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
 
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
+}
+
+
+// Make navigation labels disapear when scrolling down
+// listen for scrolling
+window.addEventListener("scroll", function(event) {
+    let windowHeight = window.pageYOffset;
+    visible = true;
+
+    if (windowHeight > 600) {
+        visible = false;
+    } else {
+        visible = true;
+    }
+
+    toggleNavButtons(visible);
+}, false);
+
+// toggle label visibility
+function toggleNavButtons(visible) {
+    if (visible) {
+        document.getElementById("about").style.visibility = "visible";
+        document.getElementById("team").style.visibility = "visible";
+        document.getElementById("contact").style.visibility = "visible";
+
+        document.getElementById("navButton").style.float = "none";
+
+        document.getElementById("homeBt").style.visibility = "hidden";
+        document.getElementById("homeBt").style.float = "none";
+    }
+    else {
+        document.getElementById("about").style.visibility = "hidden";
+        document.getElementById("team").style.visibility = "hidden";
+        document.getElementById("contact").style.visibility = "hidden";
+
+        document.getElementById("navButton").style.float = "right";
+        document.getElementById("navButton").style.margin = "10px 40px 0 0";
+
+        document.getElementById("homeBt").style.visibility = "visible";
+        document.getElementById("homeBt").style.float = "left";
+    }
 }
