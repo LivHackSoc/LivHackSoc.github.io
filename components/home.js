154dd2df7fe500e9394abf63bb0458b36367ea49
Thepnathi Stephenson <Thepnathi@hotmail.com>
2018-06-07 19:24:22 +0100

    add react component for the Q/A page

---
+++ components/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import QuestionPanel from './question-panel';
+
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
