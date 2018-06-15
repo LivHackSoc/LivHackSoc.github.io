6b328286ed98c8ec487a8b02b4cdfe6ae57ee357
Brandon <brandonskerritt51@gmail.com>
2018-06-15 22:44:00 +0100

    the checklink bot now works
    Will start to automate the other processes tomorrow :)

--- deployBot.py
+++ deployBot.py
@@ -6,32 +6,31 @@
 # compress javascript
 # compress css
 
-import requests
-import re, queue, threading
 import check_link
 from bs4 import BeautifulSoup
 import urllib.request
+from multiprocessing import Process
+
+# creates a global check_link object
+check_link_obj = check_link.check_link()
 
 def get_all_links(address):
-    resp = urllib.request.urlopen(address)
-    soup = BeautifulSoup(urllib.request.urlopen(address), from_encoding=resp.info().get_param('charset'))
-    return [link['href'] for link in soup.find_all('a', href=True)]
+    # get all links on a website, return a list
+    resp = urllib.request.urlopen("http://www.gpsbasecamp.com/national-parks")
+    soup = BeautifulSoup(urllib.request.urlopen("http://www.gpsbasecamp.com/national-parks"), from_encoding=resp.info().get_param('charset'))
+    return set([link['href'] for link in soup.find_all('a', href=True)])
 
-def threader():
-    while True:
-        value = q.get()  
-        result = extract_link(value)
-        q.task_done()
+def threader(website):
+    # this function is used to create new threads
+    check_link_obj.check(website)
+
+def main():
+    # creates new threads of threader, starts them then joins them together
+    website = input("What is the address of the website? ")
+    all_links = get_all_links(website)
+    processes = [Process(target = threader, args = (i)) for i in all_links]
+    map(lambda x: x.start(), processes)
+    map(lambda y: y.join(), processes)
 
 if __name__=="__main__":
-    colorama.init()
-    q = queue.Queue()
-    global hyperlinks, website
-    hyperlinks = set()
-    website = input("Please enter the website address: ") 
-    for x in range(30):
-        t = threading.Thread(target=threader)
-        t.deamon=True
-        t.start()   
-    q.put(website.strip())
-    q.join()
+    main()
